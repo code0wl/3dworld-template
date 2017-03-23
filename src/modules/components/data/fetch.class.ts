34efc7469c525363793d76aef801fead94281012
Oz <o.lodriguez@gmail.com>
2017-03-23 19:57:51 +0100

    canvas context

--- src/modules/components/data/fetch.class.ts
+++ src/modules/components/data/fetch.class.ts
@@ -3,6 +3,7 @@ export class DataFetch {
 
     public world: World;
     public canvas: HTMLCanvasElement = document.createElement('canvas');
+    private context: CanvasRenderingContext2D = this.canvas.getContext('2d');
 
     public constructor(world, url) {
         this.world = world;
@@ -29,17 +30,16 @@ export class DataFetch {
     };
 
     public addCanvas() {
-        // 4k
         this.canvas.width = 4096;
         this.canvas.height = 2048;
 
-        const context = this.canvas.getContext('2d');
+
 
         this.fetchJSONFile(this.world.properties.dataURL, (data) => {
 
             const container: any = document.querySelector('.data-countries');
 
-            var html = '<details> <ul>',
+            let html = '<details> <ul>',
                 x,
                 y;
 
@@ -51,14 +51,14 @@ export class DataFetch {
                     html += `<ul>`;
 
                     data.earth[key].Sales.map((key) => {
-                        html += `<li> ${key.city} <span class="total-rev"> ${key.orderValue} </span><p> Lat:  ${key.lat}, Long: ${key.lng} </p></li>`;
+                        html += `<li> ${key.city} <span class="total-rev"> ${key.orderValue} </span><p> Lat: ${key.lat}, Long: ${key.lng} </p></li>`;
 
                         x = ( (this.canvas.width / 360) * ( 180 + parseFloat(key.lng) ) );
                         y = ( (this.canvas.height / 180) * ( 90 - parseFloat(key.lat) ) );
 
-                        context.rect(x, y, this.world.properties.data_size_width, this.world.properties.data_size_height);
-                        context.fillStyle = 'orange';
-                        context.fill();
+                        this.context.rect(x, y, this.world.properties.data_size_width, this.world.properties.data_size_height);
+                        this.context.fillStyle = 'orange';
+                        this.context.fill();
                     });
 
                     html += '</ul>';

58e12fd0a1f2a810db5e6b58e26b9892a8614f4f
Oz <o.lodriguez@gmail.com>
2017-04-15 17:17:23 +0200

    dry

--- src/modules/components/ui/ui.class.ts
+++ src/modules/components/ui/ui.class.ts
@@ -1,37 +1,37 @@
-import { Clock } from '../clock/clock.class';
+import {Clock} from '../clock/clock.class';
 
 export class UI {
-
+    
     private time: Clock;
     private isShowing: boolean;
     private detail: any;
-
+    
     constructor() {
         this.time = new Clock();
     }
-
+    
     public showDetailedUI(): void {
-        this.isShowing ? this.renderUI() : this.removeUI();
+        if (this.detail) {
+            this.isShowing ? this.renderUI() : this.removeUI();
+        }
     }
-
+    
     public set showUI(show) {
         this.isShowing = show;
     }
-
+    
     public update() {
         this.time.update();
     }
-
+    
     private renderUI(): void {
         this.detail = document.querySelector('.detailed-view');
         this.detail.classList.remove('fadeOutDown');
         this.detail.classList.add('fadeInUp', 'animated');
     }
-
+    
     private removeUI(): void {
-        if (this.detail) {
-            this.detail.classList.remove('fadeInUp');
-            this.detail.classList.add('fadeOutDown');
-        }
+        this.detail.classList.remove('fadeInUp');
+        this.detail.classList.add('fadeOutDown');
     }
 }

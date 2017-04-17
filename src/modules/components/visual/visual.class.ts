17e05a795ea3d43d6c92e6d2dee970636a78f471
Oz <o.lodriguez@gmail.com>
2017-04-17 09:17:39 +0200

    dat inclusion

--- src/modules/components/visual/visual.class.ts
+++ src/modules/components/visual/visual.class.ts
@@ -1,15 +1,26 @@
 declare const dat: any;
 
 export class Visual {
-
+    
     public stats: any;
-
+    
     constructor() {
         
     }
     
     public flight() {
-        console.log('dat', dat);
+        var FizzyText = function () {
+            this.message = 'dat.gui';
+            this.speed = 0.8;
+            this.displayOutline = false;
+        };
+        
+        var text = new FizzyText();
+        var gui = new dat.GUI();
+        gui.add(text, 'message');
+        gui.add(text, 'speed', -5, 5);
+        gui.add(text, 'displayOutline');
+        
     }
     
 }
